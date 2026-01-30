3a757a074a661bd39c99f2870fc6ca548897bf0b
Hasan Abdullaev <khassanbeck@gmail.com>
2026-01-30 21:59:27 +0800

    Initial Commit

---
+++ MovieFetcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFetcher
+//
+//  Created by Hasan Abdullah on 30/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
