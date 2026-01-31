a3126f5b2f572af1a1c451ad9b76200affa4ea0f
Hasan Abdullaev <khassanbeck@gmail.com>
2026-01-31 15:22:49 +0800

    cleanup

--- MovieFetcher/Presentation/Views/MFMovieDetailViewController.swift
+++ MovieFetcher/Presentation/Views/MFMovieDetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MFMovieDetailViewController: UIViewController {
+final class MFMovieDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +15,5 @@ class MFMovieDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
