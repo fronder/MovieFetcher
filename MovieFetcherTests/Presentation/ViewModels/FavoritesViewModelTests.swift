093c20c0320af1998f35e502d56319546fea7c9a
Hasan Abdullaev <khassanbeck@gmail.com>
2026-02-03 10:13:43 +0800

    Test loading favorites should update favorite movies

---
+++ MovieFetcherTests/Presentation/ViewModels/FavoritesViewModelTests.swift
@@ -0,0 +1,41 @@
+//
+//  FavoritesViewModelTests.swift
+//  MovieFetcherTests
+//
+//  Created by Hasan Abdullah on 03/02/26.
+//
+
+import XCTest
+import Combine
+@testable import MovieFetcher
+
+@MainActor
+final class FavoritesViewModelTests: XCTestCase {
+    var sut: FavoritesViewModel!
+    var mockManageFavoritesUseCase: MockManageFavoritesUseCase!
+    var cancellables: Set<AnyCancellable>!
+    
+    override func setUp() {
+        super.setUp()
+        mockManageFavoritesUseCase = MockManageFavoritesUseCase()
+        cancellables = Set<AnyCancellable>()
+        
+        sut = FavoritesViewModel(manageFavoritesUseCase: mockManageFavoritesUseCase)
+    }
+    
+    override func tearDown() {
+        sut = nil
+        mockManageFavoritesUseCase = nil
+        cancellables = nil
+        super.tearDown()
+    }
+    
+    func testLoadFavorites_UpdatesFavoriteMovies() {
+        mockManageFavoritesUseCase.favoriteMovies = MockData.movies
+        
+        sut.loadFavorites()
+        
+        XCTAssertEqual(sut.favoriteMovies.count, 2)
+        XCTAssertEqual(mockManageFavoritesUseCase.getFavoritesCallCount, 1)
+    }
+}
